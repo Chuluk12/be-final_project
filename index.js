4b208e18b4da2c5ed57dd9d1513cc1cb7ee39ae9
Husnul <husnulchuluk08@gmail.com>
2021-06-05 20:04:03 +0700

    tes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const ProductRoutes = require('./routes/product-routes');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(function(req, res, next) {
